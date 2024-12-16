98c38093701ea77f02c5f1430c6af14da9240a3e
sort9 <143142285+sort9@users.noreply.github.com>
2024-12-16 12:50:33 -0500

    Update main.cpp
    Working on adding update functionality to employee data

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 // program global variables
 int i;
+string newFirstName, newLastName, newRole;
+double newPayRate;
 
 struct Employee {
     int employeeID;
@@ -26,19 +28,61 @@ void searchEmployee(int id);
 void searchEmployee(string firstName, string lastName);
 void searchEmployee(string role);
 void displayEmployees(void);
+void updateFirstName(string newFirstName);
+void updateLastName(string newLastName);
+void updatePayRate(double newPayRate);
+void updateRole(string newRole);
+
+updateFirstName(string newFirstName) {
+    employees[i].employeeFirstName = newFirstName;
+    cout << "Employee first name updated..." << endl;
+    cout << "Returning to main menu..." << endl;
+}
+
+updateLastName(string newLastName) {
+    // logic for updating last name
+}
+
+updatePayRate(double newPayRate) {
+    // logic for updating pay rate
+}
+
+updateRole(string newRole) {
+    // logic for updating position
+}
 
 void searchEmployee(int id) {
     bool found = false; // Flag to check if employee is found
+    int optionsChoice;
     for (int i = 0; i < employees.size(); i++) {
         if (employees[i].employeeID == id) {
             cout << "Employee(s) found: " << endl;
             cout << "ID: " << employees[i].employeeID << ", Name: " << employees[i].employeeFirstName << " " << employees[i].employeeLastName << endl;
             found = true; // Set flag to true if found
+            cout << "Options:" << endl;
+            cout << "0. Return to main menu" << endl;
+            cout << "1. Update first name" << endl;
+            cout << "2. Update last name" << endl;
+            cout << "3. Update postiion" << endl;
+            cout << "4. Update pay rate (promotion)" << endl;
+            cout << "Enter your choice: ";
+            cin >> optionsChoice;
+            switch (optionsChoice) {
+                case 0:
+                    cout << "Returning to main menu..." << endl;
+                    displayMenu();
+                case 1:
+                    cout << "Enter new name:";
+                    cin >> newFirstName;
+                    updateFirstName(string newFirstName);
+            }
             break;
         }
     }
     if (!found) {
         cout << "No employee found with ID: " << id << endl; // Message if not found
+        cout << "Returning to main menu..." << endl;
+        displayMenu();
     }
 }
 
